04610ed71f9e74231b048c9ab8d4625ea9dd0a59
YuriPanchuck <95381273+YuriPanchuck@users.noreply.github.com>
2022-12-17 20:39:15 +0200

    add profile

---
+++ components/App.js
@@ -0,0 +1,16 @@
+export var App = function App() {
+  return React.createElement(
+    'div',
+    {
+      style: {
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }
+    },
+    'React homework template'
+  );
+};
